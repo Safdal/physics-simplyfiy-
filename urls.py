0360d20fe5adc1b9de7f842ca20c229375a372df
Safdal <65144086+Safdal@users.noreply.github.com>
2024-12-31 15:04:24 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,3 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from blog import views  # Import your app views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.post_list, name='post_list'),  # Add your app's view
+]
 from django.urls import path
 from . import views
 
